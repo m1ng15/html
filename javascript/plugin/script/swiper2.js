68e9eb421fb69a3d8abb5861c0a75949fc7d4baa
m1ng115 <www.ming115@gmail.com>
2025-01-24 17:58:24 +0900

    swipter 실습

---
+++ javascript/plugin/script/swiper2.js
@@ -0,0 +1,10 @@
+//const 변수명 = new Swiper('사용자정의클래스명', {속성:값, 속성:값, });
+//const 변수명 = new Swiper('사용자정의클래스명', {속성: {속성:값, 속성:값, },});
+const bestSwiper = new Swiper('.best_swiper',{
+    //속성:'값',
+    //속성:값,
+    effect:'fade',
+    autoplay:{
+        delay:3000,
+    },
+});
