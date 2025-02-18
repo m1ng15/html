eed7dd13db6605918dfe61cdfaee65518dd50d27
www.ming115 <www.ming115@gmail.com>
2025-02-18 18:21:12 +0900

    코스알엑스 메인 페이지 업로드

--- cosrx/script/index.js
+++ cosrx/script/index.js
@@ -1,14 +1,51 @@
-const mainSwiper = new Swiper('.main_bnr',{
+const mainHeaderSwiper = new Swiper('.main_bnr',{
+    pagination:{
+        el:'main .main-pagination',
+        type: 'bullets',
+        clickable: true,
+        dynamicBullets:true,
+    },
+        navigation:{
+        nextEl:'.bnr_btn .next',
+        prevEl:'.bnr_btn .prev',
+    },
+});
+const bestSwiper = new Swiper('.best_slider',{
+    slidesPerView:5,
+    spaceBetween:20,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        type: "progressbar",
+    },
+    navigation:{
+        nextEl:'.best_btn .next',
+        prevEl:'.best_btn .prev',
+    },
+});
+const collectionSwiper = new Swiper('.collection_bnr',{
     pagination:{
         el:'main .swiper-pagination',
         type: 'bullets',
         clickable: true,
         dynamicBullets:true,
     },
-        navigation:{
-        nextEl:'.btn .next',
-        prevEl:'.btn .prev',
+});
+
+const colcProductSwiper = new Swiper('.colc_product_bnr',{
+    pagination:{
+        el:'main .colc-pagination',
+        type: 'bullets',
+        clickable: true,
+        dynamicBullets:true,
     },
 });
-const bestSwiper = new Swiper('.best_slider',{
+
+const colcProductSwiper2 = new Swiper('.colc_product_slider',{
+    slidesPerView:2.3,
+    spaceBetween:15,
+});
+
+const concernsSwiper = new Swiper('.concerns_slider',{
+    slidesPerView:3.6,
+    spaceBetween:15,
 });
