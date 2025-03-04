d0220ca362a5c5d4184f16ac5fc38663ccccff37
Min <168650608+m1ng15@users.noreply.github.com>
2025-03-04 18:15:42 +0900

    250303 코스알엑스 헤더 및 메인 베너 수정

--- cosrx/script/index.js
+++ cosrx/script/index.js
@@ -1,80 +1,15 @@
-// 0. 메인 배너 슬라이드
-const mainHeaderSwiper = new Swiper('.main_bnr',{
-    speed : 800,
-    autoplay:{delay:2000,},
-    loop: true,
-    pagination:{
-        el:'main .main-pagination',
-        type: 'bullets',
-        clickable: true,
-        dynamicBullets: true,
-    },
-    navigation:{
-        nextEl:'main .bnr_btn .next',
-        prevEl:'main .bnr_btn .prev',
-    },
-});
-// 1행 베스트 상품 슬라이드 - 전체 상품
-const bestSwiper = new Swiper('#all_best_slider',{
-    slidesPerView:5,
-    spaceBetween:20,
-    scrollbar: {
-        el: ".swiper-scrollbar",
-        type: "progressbar",
-    },
-    navigation:{
-        nextEl:'.best_btn .next',
-        prevEl:'.best_btn .prev',
-    },
-});
-// 3행 콜렉션 소개 배너
-const collectionSwiper = new Swiper('.collection_bnr',{
-    speed : 800,
-    autoplay:{delay:2000,},
-    loop: true,
-    pagination:{
-        el:'main .swiper-pagination',
-        type: 'bullets',
-        clickable: true,
-        dynamicBullets:true,
-    },
-});
-// 5행 콜렉션 상품 추천 배너
-const colcProductSwiper = new Swiper('.colc_product_bnr',{
-});
-// 5-1행 콜렉션 상품 추천 슬라이드
-const colcProductSwiper2 = new Swiper('.colc_product_slider',{
-    autoplay:{
-        delay:2000,
-        disableOnInteraction:false,
-    },
-    loop: true,
-    speed : 2000,
-    slidesPerView:2,
-    spaceBetween:15,
-});
-// 6행 피부 고민별 상품 슬라이드 - 모공/피지
-const concernsSwiper = new Swiper('#pore_slider',{
-    slidesPerView:3.6,
-    spaceBetween:15,
-});
-// 7행 베스트 리뷰 슬라이드
-const reviewSwiper = new Swiper('.review_swiper',{
-    loop: true,
-    initialSlide: 1,
-    slidesPerView: 2,
-    spaceBetween: 45,
-    centeredSlides:true,
-    on:{
-        slideChangeTransitionEnd :function(){
-            document.querySelectorAll('.review_swiper .swiper-slide').forEach(slide => slide.style.opacity = '0.2');
-            document.querySelectorAll('.review_swiper .swiper-slide').forEach(slide => slide.style.transform = 'scale(0.9)');
-            document.querySelector('.review_swiper .swiper-slide-active').style.opacity = '1';
-            document.querySelector('.review_swiper .swiper-slide-active').style.transform = 'scale(1)';
-        }
-    },
-    navigation:{
-        nextEl:'main .review_move_btn .next',
-        prevEl:'main .review_move_btn .prev',
-    },
-});
+/* 맨 위 공지사항 닫기 활성화 */
+const headerNotice = document.querySelector('.hd_top')
+const noticeClose = document.querySelector('.hd_top #close_btn')
+noticeClose.addEventListener('click',()=>{
+    headerNotice.style.display = 'none';
+})
+/* scroll에 따른 nav 색상 변환 */
+const navBar = document.querySelector('header .hd_navbar')
+navBar.classList.remove('navbar_shadow')
+window.addEventListener('scroll',()=>{
+    if ( window.pageYOffset > 800) {
+        navBar.classList.add('navbar_shadow')
+        console.log(window.pageYOffset);
+    } else { navBar.classList.remove('navbar_shadow') }
+})
